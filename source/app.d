7d08a69a0eef5c1c5bd24f0b91bf90fdf843dda3
adilbaig <adil.baig@aidezigns.com>
2015-09-21 22:19:01 +0530

    Init

---
+++ source/app.d
@@ -0,0 +1,32 @@
+import vibe.d;
+
+//shared static this()
+//{
+//	auto settings = new HTTPServerSettings;
+//	settings.port = 8080;
+//	settings.bindAddresses = ["::1", "127.0.0.1"];
+//	listenHTTP(settings, &hello);
+//
+//	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+//}
+//
+//void hello(HTTPServerRequest req, HTTPServerResponse res)
+//{
+//	res.writeBody("Hello, World!");
+//}
+
+void index(HTTPServerRequest req, HTTPServerResponse res)
+{
+    res.render!("index.dt", req);
+}
+
+shared static this()
+{
+    auto router = new URLRouter;
+    router.get("/", &index);
+
+    auto settings = new HTTPServerSettings;
+    settings.port = 8080;
+
+    listenHTTP(settings, router);
+}
